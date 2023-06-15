361de85235e202d555c327d66dd6cea92982c19c
Anabel <anastasiyabelozorovich@gmail.com>
2023-06-15 20:17:50 +0300

    task3

---
+++ homework2/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// int result = new Random().Next(100, 1000);
+// System.Console.WriteLine(result);
+// int secondnum = (result/10) % 10;
+// System.Console.WriteLine($"{secondnum}");
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// System.Console.WriteLine("Введите любое число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(number);
+// if int number > 999
+// {
+//     int result = (number/10) %10;
+//     System.Console.WriteLine(result);
+// }
+// else {
+//     int number < 100
+//     System.Console.WriteLine("В числе меньше трех цифр");
+// }
+
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Введите номер дня недели:");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day == 1) {
+    System.Console.WriteLine("Понедельник, не выходной день");
+}
+else if (day == 2) {
+    System.Console.WriteLine("Вторник, не выходной день");
+}
+else if (day == 3) {
+    System.Console.WriteLine("Среда, не выходной день");
+}
+else if (day == 4) {
+    System.Console.WriteLine("Четверг, не выходной день");
+}
+else if (day == 5) {
+    System.Console.WriteLine("Пятница, не выходной день");
+}
+else if (day == 6) {
+    System.Console.WriteLine("Суббота, выходной день");
+}
+else if (day == 7) {
+    System.Console.WriteLine("Воскресенье, выходной день");
+}
+else
+{
+System.Console.WriteLine("Такого дня не существует");
+}
